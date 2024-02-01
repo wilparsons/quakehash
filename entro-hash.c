fcc62d3c16bd50678c58e4afe488d47f71a57516
Will Parsons <entrocraft@gmail.com>
2024-02-01 21:52:13 +0000

    Adding Entro Sum to optimize collisions and distribution.

--- entro-hash.c
+++ entro-hash.c
@@ -6,13 +6,11 @@ uint32_t entro_hash(const char *input, uint32_t entropy) {
   while (input[i] != 0) {
     entropy ^= (input[i] + 111111) ^ 111111;
     entropy += (~entropy ^ 1111111111) << 4;
-    entropy -= 1111111;
     entropy = (entropy << 31) + (~entropy >> 1);
     entropy += entropy << 3;
+    entropy ^= input[i];
+    entropy += (entropy + 111111111) << 9;
     entropy += entropy << 3;
-    entropy ^= ~entropy << 10;
-    entropy ^= 111111111;
-    entropy ^= entropy << 13;
     entropy += (~entropy + entropy) << 1;
     i++;
   }
