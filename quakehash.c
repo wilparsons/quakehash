ea47c3954229a66b4f6841bea77d313e47746f65
Wil Parsons <entrocraft@gmail.com>
2024-02-27 14:41:42 -0800

    Passing all avalanche tests by mixing the high 16 to 24 bits.

--- quakehash.c
+++ quakehash.c
@@ -6,7 +6,7 @@ uint32_t quakehash(const char *input, uint32_t entropy) {
   while (input[i] != 0) {
     entropy += input[i];
     entropy += (entropy + 111111111) << 9;
-    entropy += entropy << 5;
+    entropy += ((entropy << 5) + entropy) << 16;
     entropy += ((entropy << 3) + entropy) << 24;
     entropy ^= entropy >> 16;
     i++;
